2076d647ae3cb7a1a353c6921e03d2d4ddebd7a3
xxxzamz <azzamnurataullah@gmail.com>
2026-05-25 16:55:29 +0700

    upload: ios/Flutter/flutter_export_environment.sh

--- project/ios/Flutter/flutter_export_environment.sh
+++ project/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\User\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\night"
+export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\rdvsp\cursed"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.0"
-export "FLUTTER_BUILD_NUMBER=2.0.0"
+export "FLUTTER_BUILD_NAME=4.1.0"
+export "FLUTTER_BUILD_NUMBER=4.1.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
